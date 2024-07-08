9a4061929a18740a4f953d17b7a1f8987c4003ca
AspectOfTheMeows <anwandtertom3467@gmail.com>
2024-07-08 13:48:39 +1000

    Skullmask

---
+++ dist/src/mod.ts
@@ -0,0 +1,25 @@
+import { DependencyContainer } from "tsyringe";
+
+import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt/servers/DatabaseServer";
+import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
+import { ItemHelper } from "@spt/helpers/ItemHelper";
+import { BaseClasses } from "@spt/models/enums/BaseClasses";
+
+class Mod implements IPostDBLoadMod
+{
+    public postDBLoad(container: DependencyContainer): void
+    {
+        // get database from server
+        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+
+        // Get all the in-memory json found in /assets/database
+        const tables: IDatabaseTables = databaseServer.getTables();
+        const mask_id = "635267ab3c89e2112001f826";
+        tables.templates.items[mask_id]._parent = "5448e5724bdc2ddf718b4568";
+
+        
+    }
+}
+
+export const mod = new Mod();
